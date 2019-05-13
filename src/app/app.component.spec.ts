b7e0cac63453114ec4fd6508222ae368f0ad03ed
bmVsc29u <bmVsc29u@gmail.com>
2019-05-14 00:18:14 +0800

    chore(ghpages): resolve 404 issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+import { Spec } from '@chakray/utils/testing';
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+import { AppComponent as Tag } from './app.component';
 
-  it(`should have as title 'demoStudio'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demoStudio');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demoStudio!');
+Spec.tag(Tag, {
+  schemas: [NO_ERRORS_SCHEMA],
+}, (ref) => {
+  let t: Tag;
+  beforeEach(() => {
+    t = ref.tag;
   });
 });
