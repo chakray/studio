ab306438890932e852383e9232f7b8eea1f7558c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 18:24:00 +0000

    Deploy chakray/studio to github.com/chakray/studio.git:gh-pages

---
+++ assets/fonts/cloudy.ts
@@ -0,0 +1,28 @@
+import { IconSet } from '@chakray/tags';
+import { environment as env } from 'src/environments/environment';
+
+export const cloudy: IconSet = {
+  _: {
+    cssUrl: env.host.app + '/fonts/cloudy/190516.css',
+    ff: 'cloudy'
+  } as any,
+  f_clouds3: '\e900',
+  o_cloud: '\e901',
+  f_windycloud: '\e902',
+  cloud_sync: '\e903',
+  cloud_mobile: '\e904',
+  cloud_group: '\e905',
+  cloud_ham: '\e906',
+  cloud_refresh: '\e907',
+  cloud_lighten: '\e908',
+  cloud_warn: '\e909',
+  cloud_social: '\e90a',
+  cloud_plus: '\e90b',
+  clouds2: '\e90c',
+  clouds3: '\e90d',
+  foggy: '\e90e',
+  windycloud2: '\e90f',
+  windycloud: '\e910',
+  windy: '\e911',
+  rain: '\e912',
+};
